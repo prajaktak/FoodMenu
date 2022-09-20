f894cc6067846d622df894af531b7a06b304a6c4
Prajakta <prachee.j@gmail.com>
2022-09-20 12:33:26 +0200

    Initial Commit

---
+++ FoodMenu/FoodMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodMenuApp.swift
+//  FoodMenu
+//
+//  Created by Prajakta Kulkarni on 20/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct FoodMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
